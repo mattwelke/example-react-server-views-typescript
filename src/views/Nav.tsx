c7c2420e22d54a987a70335f28a3773effa5576f
Matthew Welke <mattwelke@gmail.com>
2018-03-15 18:43:15 -0400

    Updated link names in Nav.

--- src/views/Nav.tsx
+++ src/views/Nav.tsx
@@ -9,7 +9,7 @@ export default class Nav extends React.Component<Props> {
         const msg = ' <- You are here! :)';
 
         return <ul>
-            <li><a href="/">Index</a>{this.props.activeRoute === '/' ? msg : ''}</li>
+            <li><a href="/">Home</a>{this.props.activeRoute === '/' ? msg : ''}</li>
             <li><a href="/about">About</a>{this.props.activeRoute === '/about' ? msg : ''}</li>
         </ul>;
     }
